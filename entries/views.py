6a457431b4cd930f35874a613c64ea7dfeb54df2
Vishal Sreeram <sreeram.vishal@gmail.com>
2025-06-24 21:26:21 +0530

    Added Create/Update/Delete functionalities for Diary

--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import (ListView, DetailView,)
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.urls import reverse_lazy
 # Create your views here.
 
 from .models import Entry
@@ -10,3 +11,23 @@ class EntryListView(ListView):
 
 class EntryDetailView(DetailView):
     model = Entry
+
+class EntryCreateView(CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+
+    def get_success_url(self):
+        entry_id = self.kwargs['pk']
+        return reverse_lazy(
+            "entry-detail",
+            kwargs={'pk': entry_id}
+        )
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
